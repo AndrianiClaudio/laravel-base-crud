6cf301cd25b4968c689e2e6331be48c7eec7efd7
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-25 15:53:55 +0100

    salvataggio modifiche show classi btn

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -8,37 +8,30 @@
 
 @section('content')
     {{-- {{dd($comic)}} --}}
-    <ul class="list-group">
-      @foreach ($comic->toArray() as $key => $value)
-      <li class="list-group-item">
-        {{$key}} : {{$value}}
-      </li>
-      @endforeach
-    </ul>
+<ul class="list-group">
+  @foreach ($comic->toArray() as $key => $value)
+  <li class="list-group-item">
+    {{$key}} : {{$value}}
+  </li>
+  @endforeach
+</ul>
     <div class="bg-primary">
-      <a href="{{route('comics.index')}}" class="d-block text-white text-uppercase text-decoration-none p-2 text-center">
+      <a href="{{route('comics.index')}}" class="d-block text-white text-uppercase text-decoration-none py-2 text-center">
           Back to Comics
       </a>
     </div>
-    <div class="row">
-      <div class="col">
-        <div class="card-edit">
-          <form action="{{route('comics.edit',$comic)}}" method="get" class="bg-warning p-2">
-            @csrf
-            @method('GET')
-            <button type="submit" class="d-block btn btn-warning border-2 border-danger mx-auto text-uppercase text-danger">Edit this comic</button>
-          </form>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card-delete">
-          <form action="{{route('comics.destroy',$comic)}}" method="post" class="bg-danger p-2">
-            @csrf
-            @method('delete')
-            <button type="submit" class="d-block btn btn-danger border-2 border-warning mx-auto text-uppercase text-warning">Delete this comic</button>
-          </form>
-        </div>
-      </div>
-      </div>
+    <div class="card-edit py-2">
+      <form action="{{route('comics.edit',$comic)}}" method="get" class="bg-warning">
+        @csrf
+        @method('GET')
+        <button type="submit" class="d-block btn btn-warning py-2 border-2 border-danger mx-auto text-uppercase text-danger">Edit this comic</button>
+      </form>
+    </div>
+    <div class="card-delete">
+      <form action="{{route('comics.destroy',$comic)}}" method="post" class="bg-danger">
+        @csrf
+        @method('delete')
+        <button type="submit" class="d-block btn btn-danger py-2 border-2 border-warning mx-auto text-uppercase text-warning">Delete this comic</button>
+      </form>
     </div>
 @endsection
